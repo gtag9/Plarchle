1a13c3b9faa764c871463c363f0762f1fd8d8021
Jake O'Connell <peanut@peadrew.tk>
2023-10-23 21:58:55 -0400

    revamp yuhh

---
+++ loadgamelist.js
@@ -0,0 +1,17 @@
+fetch('gamelist.json')
+            .then(response => response.json())
+            .then(data => {
+                const gameList = document.getElementById('gameList');
+                data.forEach(game => {
+                    const gameItem = document.createElement('div');
+                    gameItem.innerHTML = `
+                        <div class="game">
+                            <h2>${game.name}</h2>
+                            <img src="${game.img}" alt="${game.name} Image">
+                            <a href="${game.file}" class="button">Play</a>
+                        </div>
+                    `;
+                    gameList.appendChild(gameItem);
+                });
+            })
+            .catch(error => console.error('Error loading JSON data:', error));
