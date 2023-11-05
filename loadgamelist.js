188d5df34fcae13f9ded2eae1e7f14429bad1f61
realpeanut <jacoboconnell27@gmail.com>
2023-11-04 20:02:32 -0400

    revamp v2

--- loadgamelist.js
+++ loadgamelist.js
@@ -1,18 +1,42 @@
-fetch('gamelist.json')
+function filterGames() {
+    const gameContainer = document.getElementById("game-container");
+    const searchInput = document.getElementById("pageTitle").value.toLowerCase();
+    const gameDivs = gameContainer.getElementsByClassName("game-container");
+
+    for (let gameDiv of gameDivs) {
+        const gameTitle = gameDiv.getElementsByClassName("game-title")[0].textContent.toLowerCase();
+
+        if (gameTitle.includes(searchInput)) {
+            gameDiv.style.display = "inline-block"; // Show the game div
+        } else {
+            gameDiv.style.display = "none"; // Hide the game div
+        }
+    }
+}
+
+fetch("gamelist.json")
             .then(response => response.json())
             .then(data => {
                 data.sort((a, b) => a.name.localeCompare(b.name));
-                const gameList = document.getElementById('gameList');
+                const gameContainer = document.getElementById("game-container");
                 data.forEach(game => {
-                    const gameItem = document.createElement('div');
-                    gameItem.innerHTML = `
-                        <div class="game">
-                            <h2>${game.name}</h2>
-                            <img src="${game.img}" alt="${game.name} Image">
-                            <a href="${game.file}" class="button">Play</a>
-                        </div>
-                    `;
-                    gameList.appendChild(gameItem);
+                    const gameDiv = document.createElement("a");
+                    gameDiv.href = game.file;
+                    gameDiv.classList.add("game-container");
+
+                    const gameImage = document.createElement("img");
+                    gameImage.src = game.img;
+                    gameImage.alt = game.name;
+                    gameImage.classList.add("game-image");
+
+                    const gameTitle = document.createElement("div");
+                    gameTitle.textContent = game.name;
+                    gameTitle.classList.add("game-title");
+
+                    gameDiv.appendChild(gameImage);
+                    gameDiv.appendChild(gameTitle);
+
+                    gameContainer.appendChild(gameDiv);
                 });
             })
-            .catch(error => console.error('Error loading JSON data:', error));
+            .catch(error => console.error("Error loading gamelist.json:", error));
